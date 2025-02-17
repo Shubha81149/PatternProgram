e2bbfbce41f527123c19de834ca778572ec0dfca
Shubhashree Pradhan <shubhashreepradhan58@gmail.com>
2025-02-17 20:50:08 +0530

    fo

---
+++ PatternProgram/src/com/star/pattern/DiamondPattern.java
@@ -0,0 +1,40 @@
+package com.star.pattern;
+
+import java.util.Scanner;
+
+public class DiamondPattern {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number: ");
+		int n = sc.nextInt();
+
+		int st = 1;
+		int sp = n / 2;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= sp; j++) {
+
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= st; j++) {
+
+				System.out.print("*");
+			}
+			System.out.println();
+
+			if (i <= n / 2) {
+				
+				sp--;
+				st += 2;
+			} else {
+				
+				sp++;
+				st -= 2;
+			}
+		}
+
+	}
+
+}
