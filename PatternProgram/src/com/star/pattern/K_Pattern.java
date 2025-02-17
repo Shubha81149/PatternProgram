aa733b65e68dd88c64897264ae325f17816b9f85
Shubhashree Pradhan <shubhashreepradhan58@gmail.com>
2025-02-17 20:47:45 +0530

    ele

---
+++ PatternProgram/src/com/star/pattern/K_Pattern.java
@@ -0,0 +1,34 @@
+package com.star.pattern;
+
+import java.util.Scanner;
+
+public class K_Pattern {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number: ");
+		int n = sc.nextInt();
+
+		int st = (n / 2) + 1;
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j <= st; j++) {
+
+				System.out.print("*");
+			}
+			System.out.println();
+
+			if (i <= n / 2) {
+
+				st--;
+			} else {
+
+				st++;
+			}
+		}
+
+	}
+
+}
