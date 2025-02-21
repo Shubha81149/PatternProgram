05772c87f95418448ca1557b2845afc7bc15d527
Shubhashree Pradhan <shubhashreepradhan58@gmail.com>
2025-02-21 19:10:18 +0530

    nine

---
+++ PatternProgram/src/com/hollow/pattern/HollowDiamond.java
@@ -0,0 +1,44 @@
+package com.hollow.pattern;
+
+import java.util.Scanner;
+
+public class HollowDiamond {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number: ");
+		int n = sc.nextInt();
+
+		int st = 1;
+		int sp = n / 2;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= sp; j++) {
+
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= st; j++) {
+
+				if (j == 1 || j == st) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+
+			if (i <= n / 2) {
+
+				sp--;
+				st += 2;
+			} else {
+
+				sp++;
+				st -= 2;
+			}
+		}
+
+	}
+
+}
