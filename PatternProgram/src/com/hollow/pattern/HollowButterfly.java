0c2143e033a0214084994e6c79757d017f622608
Shubhashree Pradhan <shubhashreepradhan58@gmail.com>
2025-02-21 19:11:49 +0530

    ele

---
+++ PatternProgram/src/com/hollow/pattern/HollowButterfly.java
@@ -0,0 +1,56 @@
+package com.hollow.pattern;
+
+import java.util.Scanner;
+
+public class HollowButterfly {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number: ");
+		int n = sc.nextInt();
+
+		if (n % 2 != 0)
+			n += 1;
+
+		int st = 1;
+		int sp = n - 2;
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j <= st; j++) {
+
+				if (j == 1 || j == st) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			for (int j = 1; j <= sp; j++) {
+
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= st; j++) {
+
+				if (j == 1 || j == st) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+
+			if (i < n / 2) {
+
+				sp -= 2;
+				st++;
+			} else {
+
+				sp += 2;
+				st--;
+			}
+		}
+
+	}
+
+}
