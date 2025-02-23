7f341c45099d7434bcc22827825f37c56c2ebe43
Shubhashree Pradhan <shubhashreepradhan58@gmail.com>
2025-02-23 22:52:44 +0530

    num

---
+++ PatternProgram/src/com/number/pattern/RhombusPattern.java
@@ -0,0 +1,32 @@
+package com.number.pattern;
+
+import java.util.Scanner;
+
+public class RhombusPattern {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number: ");// 3
+		int n = sc.nextInt();
+
+		int sp = n - 1;
+		int k = 1;
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= sp; j++) {
+
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= n; j++) {
+
+				System.out.print(k);
+				k++;
+			}
+			System.out.println();
+			sp--;
+		}
+
+	}
+
+}
