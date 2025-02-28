d73e2be02270be4fd0eb179e8f2b9a75ff233082
Shubhashree Pradhan <shubhashreepradhan58@gmail.com>
2025-02-28 22:56:30 +0530

    lfft

---
+++ PatternProgram/src/com/number/pattern/LeftTriangleOnlyOneNumber.java
@@ -0,0 +1,22 @@
+package com.number.pattern;
+
+import java.util.Scanner;
+
+public class LeftTriangleOnlyOneNumber {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number: ");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				
+				System.out.print("1 ");
+			}
+			System.out.println();
+		}
+
+	}
+}
