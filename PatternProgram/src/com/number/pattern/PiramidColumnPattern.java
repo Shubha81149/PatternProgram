455d6514da7f76464876dc0caed51cb4bd8af198
Shubhashree Pradhan <shubhashreepradhan58@gmail.com>
2025-03-01 21:52:04 +0530

    piracolm

---
+++ PatternProgram/src/com/number/pattern/PiramidColumnPattern.java
@@ -0,0 +1,32 @@
+package com.number.pattern;
+
+import java.util.Scanner;
+
+public class PiramidColumnPattern {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number: ");// 3
+		int n = sc.nextInt();
+
+		int st = 1;
+		int sp = n - 1;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= sp; j++) {
+
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= st; j++) {
+
+				System.out.print(j);
+
+			}
+			System.out.println();
+			sp--;
+			st += 2;
+		}
+
+	}
+}
